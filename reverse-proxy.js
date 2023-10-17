8c9418603058e83976d6dc441651c760275bc016
Nhi Ta <tathiyennhi@gmail.com>
2023-10-17 17:02:02 +0700

    upgrade to socket connection for socket servers in reverse-proxy server

--- reverse-proxy.js
+++ reverse-proxy.js
@@ -1,15 +1,68 @@
+// const express = require("express");
+// const http = require("http");
+// const httpProxy = require("http-proxy");
+
+// const app = express();
+// const server = http.createServer(app);
+// const proxy = httpProxy
+//   .createProxyServer({
+//     target: ["http://localhost:1235", "http://localhost:1234"],
+//     ws: true,
+//   })
+//   .listen(8080);
+
+// app.use("/socket/server1", (req, res) => {
+//   proxy.web(req, res, {
+//     target: "http://localhost:1234",
+//     pathRewrite: { "^/socket/server1": "/" },
+//   });
+// });
+
+// app.use("/socket/server1", (req, res) => {
+//   proxy.web(req, res, {
+//     target: "http://localhost:1235",
+//     pathRewrite: { "^/socket/server2": "/" },
+//   });
+// });
+
+// app.use("/backend", (req, res) => {
+//   proxy.web(req, res, {
+//     target: "http://localhost:9797",
+//   });
+// });
+
+// app.use("/backend1", (req, res) => {
+//   proxy.web(req, res, {
+//     target: "http://localhost:9798",
+//     ws: true,
+//   });
+// });
+
+// proxy.on("error", (err, req, res) => {
+//   console.error("Proxy Error:", err);
+//   res.status(500).send("Proxy Error");
+// });
+
+// // Upgrade WebSocket
+// server.on("upgrade", (req, socket, head) => {
+//   proxy.ws(req, socket, head, { target: "http://localhost:1235" });
+// });
+
+// // server.on("upgrade", (req, socket, head) => {
+// //   proxy.ws(req, socket, head, { target: "http://localhost:1234" });
+// // });
+// const port = 9090;
+// server.listen(port, () => {
+//   console.log(`Proxy Server listening on port ${port}`);
+// });
+
 const express = require("express");
 const http = require("http");
 const httpProxy = require("http-proxy");
 
 const app = express();
 const server = http.createServer(app);
-const proxy = httpProxy
-  .createProxyServer({
-    target: "http://localhost:1234",
-    ws: true,
-  })
-  .listen(8080);
+const proxy = httpProxy.createProxyServer({ ws: true });
 
 app.use("/socket/server1", (req, res) => {
   proxy.web(req, res, {
@@ -21,30 +74,31 @@ app.use("/socket/server1", (req, res) => {
 app.use("/socket/server2", (req, res) => {
   proxy.web(req, res, {
     target: "http://localhost:1235",
+    pathRewrite: { "^/socket/server2": "/" },
   });
 });
 
-app.use("/backend", (req, res) => {
-  proxy.web(req, res, {
-    target: "http://localhost:9797",
-  });
+// Hàm xử lý upgrade linh hoạt
+server.on("upgrade", (req, socket, head) => {
+  const target = selectTarget(req);
+  proxy.ws(req, socket, head, { target });
 });
 
-app.use("/backend1", (req, res) => {
-  proxy.web(req, res, {
-    target: "http://localhost:9798",
-    ws: true,
-  });
-});
+// Hàm chọn máy chủ WebSocket dựa trên đường dẫn
+const selectTarget = (req) => {
+  console.log(req);
+  if (req.url.startsWith("/socket/server1")) {
+    return "http://localhost:1234";
+  } else if (req.url.startsWith("/socket/server2")) {
+    return "http://localhost:1235";
+  }
+  // Nếu không trùng với bất kỳ đường dẫn nào khác, mặc định là máy chủ đầu tiên
+  return "http://localhost:1234";
+};
 
 proxy.on("error", (err, req, res) => {
   console.error("Proxy Error:", err);
   res.status(500).send("Proxy Error");
-});
-
-// Upgrade WebSocket
-server.on("upgrade", (req, socket, head) => {
-  proxy.ws(req, socket, head, { target: "http://localhost:1234" });
 });
 
 const port = 9090;
--- socket-server-2.js
+++ socket-server-2.js
@@ -23,3 +23,58 @@
 // server.listen(port, () => {
 //   console.log(`Server listening on port ${port}`);
 // });
+// const http = require("http");
+// const socketIO = require("socket.io");
+
+// const server = http.createServer();
+// const io = socketIO(server);
+
+// io.on("connection", (socket) => {
+//   console.log("Client connected");
+
+//   // Gửi sự kiện "hello" mỗi 5 giây
+//   const interval = setInterval(() => {
+//     socket.emit("hello", "Hello from server 1!");
+//   }, 5000);
+
+//   // Ngắt kết nối khi client ngắt kết nối
+//   socket.on("disconnect", () => {
+//     console.log("Client disconnected");
+//     clearInterval(interval);
+//   });
+// });
+
+// const port = 1234;
+// server.listen(port, () => {
+//   console.log(`Server listening on port ${port}`);
+// });
+
+const http = require("http");
+const socketIO = require("socket.io");
+
+const server = http.createServer();
+const io = socketIO(server);
+
+// Cấu hình cho Socket.IO
+const ioPath = "/socket/server2";
+const ioNamespace = io.of(ioPath);
+
+ioNamespace.on("connection", (socket) => {
+  console.log("Client connected");
+
+  // Gửi sự kiện "hello" mỗi 5 giây
+  const interval = setInterval(() => {
+    socket.emit("hello", "Hello from server 2!");
+  }, 5000);
+
+  // Ngắt kết nối khi client ngắt kết nối
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+});
+
+const port = 1235;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
